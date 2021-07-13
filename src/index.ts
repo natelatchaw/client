caafc67b12bc137c9e8c8e4f51a238f9a5055006
Nathan Latchaw <natelatchaw@gmail.com>
2021-07-13 01:16:14 -0500

    updated to utilize onDispatch

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,39 @@
-import { Console } from 'discord-bot/dist';
-import { Core } from 'discord-bot/dist';
-import { File } from 'discord-bot/dist';
-import { Dispatch } from 'discord-bot/dist/models/payloads/dispatch'
-import { GuildCreate } from 'discord-bot/dist';
-import { Heartbeat } from 'discord-bot/dist';
+import {
+  Console,
+  Core,
+  Dispatch,
+  Event,
+} from 'discord-bot/dist';
+import { File } from './models/file';
 import dotenv from 'dotenv';
 import { URL } from 'url';
 
 /**
  * Open the connection to Discord
  */
- async function start() {
-    try {
-      const file: File = new File('./.env');
-      await file.open();
-      await file.close();
-    }
-    catch (error: any) {
-      if (error.code === 'EEXIST') { } else throw error;
-    }
-  
-    dotenv.config();
-    const endpoint: URL = new URL('wss://gateway.discord.gg/?v=9&encoding=json');
-    const token: string = process.env.TOKEN ?? '';
-    Console.log(`Token: ${token}`);
-    const core: Core = new Core(endpoint, token);
-    console.log(core);
-    core.onDispatch((dispatch: Dispatch<Event>) => {
-      Console.highlight(dispatch.t);
-    });
-    
+async function start() {
+  try {
+    const file: File = new File('./.env');
+    await file.open();
+    await file.close();
   }
-  
-  start()
-      .then(() => console.log('Complete.'))
-      .catch((error: Error) => console.error(error));
-  
+  catch (error: any) {
+    if (error.code === 'EEXIST') { } else throw error;
+  }
+
+  dotenv.config();
+  const endpoint: URL = new URL('wss://gateway.discord.gg/?v=9&encoding=json');
+  const token: string = process.env.TOKEN ?? '';
+  Console.log(`Token: ${token}`);
+  const core: Core = new Core(endpoint, token);
+  core.onDispatch((dispatch: Dispatch<Event>) => {
+    // if the dispatch's event code is undefined, return
+    if (dispatch.t == undefined) return;
+    const event: Event = dispatch.d;
+    console.log(event);
+  });
+}
+
+start()
+    .then(() => console.log('Complete.'))
+    .catch((error: Error) => console.error(error));
